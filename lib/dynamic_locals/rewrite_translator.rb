08d18cd8c40dfd693f38e6947253ab46dd616031
John Hawthorn <john@hawthorn.email>
2019-06-15 20:53:17 -0700

    Fix replacements on multiple lines

--- lib/dynamic_locals/rewrite_translator.rb
+++ lib/dynamic_locals/rewrite_translator.rb
@@ -5,20 +5,38 @@ module DynamicLocals
     def translate(original_src)
       root = RubyVM::AbstractSyntaxTree.parse(original_src)
       vcalls = extract_vcalls(root)
-      vcalls = vcalls.sort_by(&:last_column).reverse
+
+      line_offsets = [0]
+      idx = 0
+      while idx = original_src.index("\n", idx)
+        idx += 1
+        line_offsets << idx
+      end
+
+      vcalls = vcalls.sort_by { |x| range_from(line_offsets, x).end }.reverse
 
       src = original_src.dup
       vcalls.each do |vcall|
         name = vcall.children[0]
-        range = (vcall.first_column)...(vcall.last_column)
+        range = range_from(line_offsets, vcall)
         original = src[range]
-        src[(vcall.first_column)...(vcall.last_column)] = "locals.fetch(#{name.inspect}){ #{name}() }"
+        src[range] = "locals.fetch(#{name.inspect}){ #{name}() }"
       end
 
       src
     end
 
     private
+
+    def position_from(line_offsets, line, column)
+      line_offsets[line-1] + column
+    end
+
+    def range_from(line_offsets, node)
+      first = position_from(line_offsets, node.first_lineno, node.first_column)
+      last  = position_from(line_offsets, node.last_lineno, node.last_column)
+      first...last
+    end
 
     def extract_vcalls(node)
       return [] unless RubyVM::AbstractSyntaxTree::Node === node
