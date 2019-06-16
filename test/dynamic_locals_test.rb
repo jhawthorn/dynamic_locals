8e6a46fea85eca250a7210d7b6e9faedca94f952
John Hawthorn <john@hawthorn.email>
2019-06-15 19:04:10 -0700

    More tests

--- test/dynamic_locals_test.rb
+++ test/dynamic_locals_test.rb
@@ -14,12 +14,32 @@ module CommonBehaviour
     assert_equal expected, actual, "Expected #{actual.inspect} to equal #{expected.inspect}"
   end
 
+  def my_method_name
+    :hi_from_my_method
+  end
+
   def test_no_variables
     assert_dynamic_result(2, "1+1")
   end
 
   def test_variable_access
     assert_dynamic_result(2, "foo", { foo: 2 })
+  end
+
+  def test_method_call
+    assert_dynamic_result(:hi_from_my_method, "my_method_name", { foo: 2 })
+  end
+
+  def test_variable_access_shadowing_method
+    assert_dynamic_result(:a_local, "my_method_name", { my_method_name: :a_local })
+  end
+
+  def test_method_call_parens
+    assert_dynamic_result(:hi_from_my_method, "my_method_name()", { my_method_name: :a_local })
+  end
+
+  def test_method_call_block
+    assert_dynamic_result(:hi_from_my_method, "my_method_name {}", { my_method_name: :a_local })
   end
 end
 
